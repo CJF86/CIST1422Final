6f24d4bf6532dd7ff09fd609d2e4ad81e7c7188f
CJF86 <77642189+CJF86@users.noreply.github.com>
2022-10-24 16:07:18 -0400

    Ninth_CheckIn
    Added full forwards/backwards functionality to character selection buttons. Created next scene and added play button to load it on click

--- My project/Assets/Scripts/Character_Cycling.cs
+++ My project/Assets/Scripts/Character_Cycling.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Character_Cycling : MonoBehaviour
 {
+    //This script handles the button events used for selecting the player character
+    //This script also handles the play button and is used to load the next scene/save player character choice
+
     public GameObject[] Character_Array;
 
     public GameObject Character_Spot;
@@ -27,9 +31,9 @@ public class Character_Cycling : MonoBehaviour
 
         Player_Character = Character_Array[Character_Selection];
 
-        Character = Instantiate(Player_Character) as GameObject;
+        Character = Instantiate(Player_Character,Pos_Trans, Quaternion.identity) as GameObject;
 
-
+        Character.transform.Rotate(Vector3.up, 180);
     }
     public void Forward_Choice()
     {
@@ -47,7 +51,36 @@ public class Character_Cycling : MonoBehaviour
 
         //Character.transform.Translate(Vector3.up * -15);
 
-        Debug.Log("Button being pressed");
+        Debug.Log("Forward Button being pressed");
+
+    }
+
+    public void Backward_Choice()
+    {
+        Player_Character = Character_Array[Character_Selection];
+
+        Destroy(Character);
+
+        Character_Selection = (Character_Selection - 1);
+
+        if(Character_Selection < 0)
+        {
+            Character_Selection += Character_Array.Length;
+        }
+
+        Character = Instantiate(Player_Character, Pos_Trans, Quaternion.identity) as GameObject;
+
+        //Character.transform.position = Pos_Trans;
+
+        Character.transform.Rotate(Vector3.up, 180);
+
+        Debug.Log("Backward Button being pressed");
+    }
+
+    public void Start_Game()
+    {
+        PlayerPrefs.SetInt("Character_Selection", Character_Selection);
+        SceneManager.LoadScene("Prison_Scene",LoadSceneMode.Single);
 
     }
 
